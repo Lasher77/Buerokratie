9bb77d73d5d4d8ec755c8f4f52a8ca8c489f7036
Lasher77 <sven.winkler@gmx.net>
2025-07-21 14:28:16 +0200

    Add API_BASE constant and update report form

---
+++ frontend/src/components/__tests__/ReportForm.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+
+let ReportForm;
+
+jest.mock('axios', () => ({
+  __esModule: true,
+  default: {
+    post: jest.fn(),
+  },
+}));
+
+jest.mock('../CategorySelect', () => ({ value, onChange }) => (
+  <select data-testid="category-select" value={value} onChange={e => onChange(e.target.value)}>
+    <option value="">-- Bitte wählen --</option>
+    <option value="1">Category 1</option>
+  </select>
+));
+
+beforeEach(() => {
+  process.env.REACT_APP_API_BASE_URL = '/base';
+  jest.resetModules();
+  ReportForm = require('../ReportForm').default;
+});
+
+test('sends data to correct base url', async () => {
+  axios.post.mockResolvedValueOnce({ data: {} });
+  render(<ReportForm />);
+
+  await userEvent.type(screen.getByLabelText(/Titel/), 'Test');
+  await userEvent.type(screen.getByLabelText(/Beschreibung/), 'Desc');
+  await userEvent.selectOptions(screen.getByTestId('category-select'), '1');
+  await userEvent.click(screen.getByRole('button', { name: /absenden/i }));
+
+  await waitFor(() => expect(axios.post).toHaveBeenCalled());
+  expect(axios.post).toHaveBeenCalledWith('/base/api/reports', expect.any(Object));
+});
